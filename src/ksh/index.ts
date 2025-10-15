8bd50606b7092a1440ae8763b3c7dddb9163090c
Jimin Park <pjm950@gmail.com>
2025-10-16 00:39:05 +0900

    Added skeleton API for KSH.

--- src/ksh/index.ts
+++ src/ksh/index.ts
@@ -1,7 +1,10 @@
-export function parseKSH(): never {
+import { KSON } from "../kson";
+
+export function parseKSH(): KSON {
     throw new Error("Not yet implemented!");
 }
 
-export function stringifyKSH(): never {
-    throw new Error("Not yet implemented!");
+export function stringifyKSH(kson: KSON): never {
+    if(kson) throw new Error("Not yet implemented!");
+    else return kson as never;
 }
