41c3a781977655eedc90691b7086c98337e5d3da
Jimin Park <pjm950@gmail.com>
2025-10-22 19:24:59 +0900

    Initialize with an empty KSON.

--- src/ksh/test/02-nov.spec.ts
+++ src/ksh/test/02-nov.spec.ts
@@ -4,10 +4,10 @@ import { parseKSH, PULSES_PER_WHOLE } from "../ast/index.js";
 import { readTestData } from "../../util/test.js";
 
 import { ksh2kson } from "../converter/index.js";
-import { KSON, KSON_VERSION } from "../../kson/index.js";
+import { KSON, KSON_VERSION, createKSON } from "../../kson/index.js";
 
 describe("ksh/test/02-nov", function() {
-    let kson: KSON|null = null;
+    let kson: KSON = createKSON();
 
     before(async function() {
         const data = await readTestData('chart/testcase/02-nov.ksh');
@@ -17,12 +17,10 @@ describe("ksh/test/02-nov", function() {
     });
 
     it("should set the version correctly", function() {
-        assert.exists(kson);
         assert.strictEqual(kson.version, KSON_VERSION);
     });
 
     it("should set the metadata correctly", function() {
-        assert.exists(kson);
         assert.deepStrictEqual(kson.meta, {
             title: "Testcase 02 [NOV]",
             artist: "02-NOV art HEXAGON",
@@ -36,14 +34,14 @@ describe("ksh/test/02-nov", function() {
     });
 
     it("should set the beat info correctly", function() {
-        assert.exists(kson?.beat);
+        assert.exists(kson.beat);
         assert.deepStrictEqual(kson.beat.bpm, [[0, 120]]);
         assert.deepStrictEqual(kson.beat.time_sig, [[0, [4, 4]]]);
         assert.deepStrictEqual(kson.beat.scroll_speed, [[0, [1.0, 1.0]]]);
     });
 
     it("should set the note info correctly", function() {
-        assert.exists(kson?.note);
+        assert.exists(kson.note);
         const note = kson.note;
 
         assert.deepStrictEqual(note.bt[0], [
