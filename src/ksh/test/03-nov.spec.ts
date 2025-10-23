6d7f46285605d7045ce91c02406ba2fb9b50fcd3
Jimin Park <pjm950@gmail.com>
2025-10-23 12:56:48 +0900

    Implemented `createLaserSection`.

--- src/ksh/test/03-nov.spec.ts
+++ src/ksh/test/03-nov.spec.ts
@@ -1,17 +1,31 @@
 import { assert } from 'chai';
 
-import { parseKSH } from "../ast/index.js";
+import { parseKSH, Pulse, PULSES_PER_WHOLE } from "../ast/index.js";
 import { readTestData } from "../../util/test.js";
 
 import { ksh2kson } from "../converter/index.js";
-import { KSON, LaserSection, createKSON } from "../../kson/index.js";
+import { GraphSectionPointArray, KSON, LaserSection, createKSON } from "../../kson/index.js";
+
+const PULSES_PER_BEAT = PULSES_PER_WHOLE / 4;
 
 function createLaserSection(points: Array<[beat_no: number, pos: number]>): LaserSection {
-    // TODO
-    return [
-        0,
-        [],
-    ];
+    if(points.length === 0) return [0, []];
+
+    const [start_beat] = points[0];
+    const y: Pulse = start_beat * PULSES_PER_BEAT;
+
+    const v: GraphSectionPointArray = [];
+    for(const [beat_no, pos] of points) {
+        const ry = (beat_no - start_beat) * PULSES_PER_BEAT;
+        const last_v = v.at(-1);
+        if(last_v?.[0] === ry) {
+            last_v[1][1] = pos;
+        } else {
+            v.push([ry, [pos, pos]]);
+        }
+    }
+
+    return [y, v];
 }
 
 describe("ksh/test/03-nov", function() {
